4df6cb438465e973644765e209c11093eacfd7ec
Ananth Vankipuram <vsap78@gmail.com>
2021-01-11 22:08:52 -0800

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, Fragment } from "react";
+import Tabletop from "tabletop";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Tabletop.init({
+      key: "1HO30hT95TzjLslOMRDx8jnCTqMrFJ3HYL9MEBfoYuXk",
+      simpleSheet: true
+    })
+      .then((data) => setData(data))
+      .catch((err) => console.warn(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>LUPTON CAPITAL ALLOCATION TABLE</h1>
+      <ul>
+      {data.map((item, i) => (
+        <Fragment key={i}>
+          <li>SYMBOL  {item.SYMBOL}</li>
+          <li>ALLOCATION  {item.ALLOCATION}</li>
+          <li>PRICE  {item.PRICE}</li>
+          <li>CHANGE  {item.CHANGE}</li>
+          <br />
+        </Fragment>
+      ))}
+    </ul>
+    <h1>JANUARY PERFORMANCE</h1>
+    <ul>
+      <li>JONAH = 24.5%</li>
+      <li>DOW = 1.60%</li>
+      <li>NASDAQ = 2.43%</li>
+      <li>S&P = 1.81%</li>
+      <li> RUSSELL = 7.34%</li>
+    </ul>
+    </>
   );
 }
-
-export default App;
